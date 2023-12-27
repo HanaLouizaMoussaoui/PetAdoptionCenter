e3e1b9ed645011de471c3d0bb9194b25dd2d9362
TarynBeaupre <100823889+TarynBeaupre@users.noreply.github.com>
2023-12-27 01:26:21 -0500

    Added classes for the 3 added pets: Bird, Cat. Dragon

---
+++ PIIIProject/Models/Dragon.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PIIIProject.Models
+{
+    internal class Dragon : Pet
+    {
+        #region Fields
+        private string[] FAVORITE_ACTIVITIES = { "Breathe Fire", "Flying", "BBQing" };
+        private string _favoriteGame;
+        #endregion
+        #region Constructors
+        public Dragon(string name, int age, bool isAdopted) : base(name, age, isAdopted)
+        {
+            FavoriteGame = RandomTraitPicker(FAVORITE_ACTIVITIES);
+        }
+        #endregion
+        #region Properties
+        public string FavoriteGame
+        {
+            get { return _favoriteGame; }
+            set { _favoriteGame = value; }
+        }
+        #endregion
+    }
+}
