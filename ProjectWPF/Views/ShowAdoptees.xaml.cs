37ff14fd30e96c4bcad2ab0bd4a24d2d38d947e6
Hana <123187541+HanaLouizaMoussaoui@users.noreply.github.com>
2023-12-30 12:10:18 -0500

    Added comments to the show adoptees code

--- ProjectWPF/Views/ShowAdoptees.xaml.cs
+++ ProjectWPF/Views/ShowAdoptees.xaml.cs
@@ -28,31 +28,44 @@ namespace ProjectWPF.Views
         }
         private void DisplayAdoptees()
         {
+            // Fetching the adoptees we have on file
             List<Adoptee> adoptees = AdopteeDatabase.RetrieveAdopteeDatabase();
+
+            // Fetching our textblocks from the window 
             TextBlock[] adopteeNameTextBlocks = {adoptee1Name, adoptee2Name, adoptee3Name, adoptee4Name};
-            TextBlock[] adopteePetTextBlocks = { adoptee1Pets, adoptee2Pets, adoptee3Pets, adoptee4Pets };
+            TextBlock[] adopteePetTextBlocks = {adoptee1Pets, adoptee2Pets, adoptee3Pets, adoptee4Pets};
+
+            // If there are no adoptees we show a message
             if (adoptees.Count==0)
             {
                 adopteeNameTextBlocks[0].Text = "No one has adopted a pet yet.";
             }
+
+            // Going through each adoptee
             for (int i = 0; i < adoptees.Count && i < adopteeNameTextBlocks.Length; i++)
             {
+                // Displaying the adoptee's name
                 adopteeNameTextBlocks[i].Text = $"{adoptees[i].Name}";
+                // Using a string builder for showing the adoptee's pets
                 StringBuilder sb = new StringBuilder();
-                sb.Append("Adopted ");
-                sb.Append($"{adoptees[i].AdoptedPets[0].Name} ");
-        
-                    for (int j = 1; j < adoptees[i].AdoptedPets.Count; j++)
-                    {
-                        sb.Append($"and ");
-                        string petName = adoptees[i].AdoptedPets[j].Name;
-                        sb.Append($"{petName} ");
-                    }
 
+                // Appending their first pet (since all adoptees will have at least one pet)
+                sb.Append("Adopted ");
+                sb.Append($"{adoptees[i].AdoptedPets[0].Name}");
+
+                // If this adoptee has adopted more than one pet, we append the other pet names
+                for (int j = 1; j < adoptees[i].AdoptedPets.Count; j++)
+                {
+                    sb.Append($"and ");
+                    string petName = adoptees[i].AdoptedPets[j].Name;
+                    sb.Append($"{petName} ");
+                }
+                // Displaying the adoptee's pet(s)
                 adopteePetTextBlocks[i].Text = sb.ToString();
             }
 
         }
+        // Allows the user to return to the main window
         public void BtnClick_GoBackMain(object sender, RoutedEventArgs e)
         {
             MainWindow newMain = new MainWindow();
