e3c95b4dec70808b942991ab880729033668bdc3
Taryn Beaupre <2173710@johnabbottcollege.net>
2024-01-02 23:56:33 -0500

    Created two views to show adopted and available pets. Added some comments to the mainwindow methods

--- ProjectWPF/MainWindow.xaml.cs
+++ ProjectWPF/MainWindow.xaml.cs
@@ -24,9 +24,10 @@ namespace ProjectWPF
     public partial class MainWindow : Window
     {
         private const int MAX_PETS = 12;
+        private const int MAX_DISPLAYFRAMES = 4;
         //note the 50 here is a placeholder
-        private int[] alreadyGeneratedPets = new int[4] { 50, 50, 50, 50 };
-        private int[] previousGeneratedPets = new int[4];
+        private int[] alreadyGeneratedPets = new int[MAX_DISPLAYFRAMES] { 50, 50, 50, 50 };
+        private int[] previousGeneratedPets = new int[MAX_DISPLAYFRAMES];
         private bool showingOldPetsAgain = false;
         public MainWindow()
         {
@@ -44,6 +45,7 @@ namespace ProjectWPF
         {
             ShowPets();
         }
+
         private void Pet_Selected(object sender, RoutedEventArgs e)
         {
             int petIndex = 0;
@@ -71,8 +73,8 @@ namespace ProjectWPF
         }
         private Pet[] Random_Pets_Generator()
         {
-            alreadyGeneratedPets = new int[4] { 50, 50, 50, 50 };
-            Pet[] pets = new Pet[4];
+            alreadyGeneratedPets = new int[MAX_DISPLAYFRAMES] { 50, 50, 50, 50 };
+            Pet[] pets = new Pet[MAX_DISPLAYFRAMES];
             Random randomPetSelector = new Random();
             for (int i = 0; i < pets.Length; i++)
             {
@@ -86,9 +88,15 @@ namespace ProjectWPF
             }
             return pets;
         }
+
+        /// <summary>
+        /// Shows pets previously generated on the page if the user returns to the main screen from the pet details page.
+        /// </summary>
+        /// <param name="petsToGenerate">An array containing the index of the previously generated pets.</param>
+        /// <returns>An array of 4 pets that were previously generated.</returns>
         private Pet[] PreviousPetsGenerator(int[] petsToGenerate)
         {
-            Pet[] pets = new Pet[4];
+            Pet[] pets = new Pet[MAX_DISPLAYFRAMES];
             for (int i = 0; i < pets.Length; i++)
             {
                 int petIndex = petsToGenerate[i];
@@ -96,11 +104,28 @@ namespace ProjectWPF
             }
             return pets;
         }
+
+        /// <summary>
+        /// Displays 4 randomized pets and their images. Also makes 2 buttons appear to filter the generated pets.
+        /// </summary>
         private void ShowPets()
         {
             seePetsButton.Content = "Refresh to see more pets!";
             seeAvailablePetsButton.Visibility = Visibility.Visible;
             seeAdoptedPetsButton.Visibility = Visibility.Visible;
+            Pet[] petsToShow = GetPetsToShow();
+            pet1Photo.Source = new BitmapImage(new Uri($"/Images/{petsToShow[0].Name}.png", UriKind.Relative));
+            pet1Name.Text = petsToShow[0].Name;
+            pet2Photo.Source = new BitmapImage(new Uri($"/Images/{petsToShow[1].Name}.png", UriKind.Relative));
+            pet2Name.Text = petsToShow[1].Name;
+            pet3Photo.Source = new BitmapImage(new Uri($"/Images/{petsToShow[2].Name}.png", UriKind.Relative));
+            pet3Name.Text = petsToShow[2].Name;
+            pet4Photo.Source = new BitmapImage(new Uri($"/Images/{petsToShow[3].Name}.png", UriKind.Relative));
+            pet4Name.Text = petsToShow[3].Name;
+        }
+
+        private Pet[] GetPetsToShow()
+        {
             Pet[] petsToShow;
             if (showingOldPetsAgain)
             {
@@ -111,15 +136,66 @@ namespace ProjectWPF
             {
                 petsToShow = Random_Pets_Generator();
             }
-            pet1photo.Source = new BitmapImage(new Uri($"/Images/{petsToShow[0].Name}.png", UriKind.Relative));
-            pet1Name.Text = petsToShow[0].Name;
-            pet2photo.Source = new BitmapImage(new Uri($"/Images/{petsToShow[1].Name}.png", UriKind.Relative));
-            pet2Name.Text = petsToShow[1].Name;
-            pet3photo.Source = new BitmapImage(new Uri($"/Images/{petsToShow[2].Name}.png", UriKind.Relative));
-            pet3Name.Text = petsToShow[2].Name;
-            pet4photo.Source = new BitmapImage(new Uri($"/Images/{petsToShow[3].Name}.png", UriKind.Relative));
-            pet4Name.Text = petsToShow[3].Name;
+            return petsToShow;
         }
+
+        /// <summary>
+        /// Shows the pets that are available for adoption from the 4 generated options.
+        /// </summary>
+        private void Btn_Click_ShowAvailablePets(object sender, RoutedEventHandler e)
+        {
+            ShowAvailablePets();
+        }
+
+        private void ShowAvailablePets()
+        {
+Pet[] petsToShow = GetPetsToShow();
+            int usedDisplayFrames = 0; //this variable will hold the number of used display frames for the pets (max 4) 
+            for (int i = 0; i <  petsToShow.Length; i++)
+            {
+                if (petsToShow[i].IsAdopted == false )
+                {
+                    switch (usedDisplayFrames)
+                    {
+                        case 1:
+                            pet1Photo.Source = new BitmapImage(new Uri($"/Images/{petsToShow[i].Name}.png", UriKind.Relative));
+                            pet1Name.Text = petsToShow[i].Name;
+                            break;
+                        case 2:
+                            pet2Photo.Source = new BitmapImage(new Uri($"/Images/{petsToShow[i].Name}.png", UriKind.Relative));
+                            pet2Name.Text = petsToShow[i].Name;
+                            break;
+                        case 3:
+                            pet3Photo.Source = new BitmapImage(new Uri($"/Images/{petsToShow[i].Name}.png", UriKind.Relative));
+                            pet3Name.Text = petsToShow[i].Name;
+                            break;
+                        case 4:
+                            pet4Photo.Source = new BitmapImage(new Uri($"/Images/{petsToShow[i].Name}.png", UriKind.Relative));
+                            pet4Name.Text = petsToShow[i].Name;
+                            break;
+                    }
+                    usedDisplayFrames++;
+                }
+            }
+            if (usedDisplayFrames == 0 ) 
+            {
+                pet1Name.Text = "None of these pets are available for adoption.";
+            }
+        }
+
+        /// <summary>
+        /// Shows the pets that have been adopted from the 4 generated options
+        /// </summary>
+        private void Btn_Click_ShowAdoptedPets(object sender, RoutedEventHandler e)
+        {
+            Pet[] petsToShow = GetPetsToShow();
+        }
+
+        /// <summary>
+        /// Opens a new window displaying 4 adopters. Closes the currently opened window.
+        /// </summary>
+        /// <param name="sender">The calling object (in this case the main window).</param>
+        /// <param name="e"></param>
         private void Btn_Click_Show_Adopters(object sender, RoutedEventArgs e)
         {    
             ShowAdopters adoptees = new ShowAdopters();
